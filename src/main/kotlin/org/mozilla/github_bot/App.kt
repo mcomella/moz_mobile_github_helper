4c9b4dc309c53bc36bffb79c52bf2df1ebd76f45
Michael Comella <michael.l.comella@gmail.com>
2017-12-09 11:37:33 -0800

    Switch to Application.

--- src/main/kotlin/org/mozilla/github_bot/App.kt
+++ src/main/kotlin/org/mozilla/github_bot/App.kt
@@ -1,23 +1,20 @@
 package org.mozilla.github_bot
 
+import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.http.ContentType
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.response.respondText
-import io.ktor.routing.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import io.ktor.routing.Routing
+import io.ktor.routing.get
 
-private const val PORT_PROPERTY = "server.port"
-
-fun main(args: Array<String>) {
-    val port = System.getProperty(PORT_PROPERTY, "8080").toInt()
-
-    val server = embeddedServer(Netty, port) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello world")
         }
     }
-    server.start(wait = true)
 }
