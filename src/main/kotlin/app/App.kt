4f284d448b3d8b286c68d8b60504ea5ca2f07e0b
Michael Comella <michael.l.comella@gmail.com>
2017-12-08 22:14:01 -0800

    Add ktor quickstart.

--- src/main/kotlin/app/App.kt
+++ src/main/kotlin/app/App.kt
@@ -1,7 +1,13 @@
 package app
 
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 fun main(args: Array<String>) {
-    /*
     val server = embeddedServer(Netty, 8080) {
         routing {
             get("/") {
@@ -10,6 +16,4 @@ fun main(args: Array<String>) {
         }
     }
     server.start(wait = true)
-    */
-    print("lol")
 }
