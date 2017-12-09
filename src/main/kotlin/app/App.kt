ae14d637020af3603b6af2c6dbbb37e201696fda
Michael Comella <michael.l.comella@gmail.com>
2017-12-08 22:12:11 -0800

    Add ktor; full kotlin.

---
+++ src/main/kotlin/app/App.kt
@@ -0,0 +1,15 @@
+package app
+
+fun main(args: Array<String>) {
+    /*
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+    */
+    print("lol")
+}
