e4d71de43a0a64d89262f4e6ddea166d8c48b5a3
Michael Comella <michael.l.comella@gmail.com>
2017-12-10 14:24:59 -0800

    Delegate PR request based on action type; refactor App

--- src/main/kotlin/org/mozilla/github_bot/App.kt
+++ src/main/kotlin/org/mozilla/github_bot/App.kt
@@ -3,11 +3,8 @@ package org.mozilla.github_bot
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.content.readText
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.HttpStatusCode
-import io.ktor.response.respond
 import io.ktor.routing.post
 import io.ktor.routing.routing
 
@@ -15,12 +12,7 @@ fun Application.main() {
     installFeatures()
     routing {
         post("/") {
-            val payload = call.request.receiveContent().readText()
-            if (!GithubWebhookAuth.isRequestAuthorized(EnvVar, call.request.headers, payload)) {
-                call.respond(HttpStatusCode.Forbidden)
-            } else {
-                call.respond(HttpStatusCode.OK)
-            }
+            GithubWebhook.handle(call)
         }
     }
 }
