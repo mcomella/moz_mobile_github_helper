11a2c6c806667e0fae9ec5ba5dc5c22e3d6ccea8
Michael Comella <michael.l.comella@gmail.com>
2017-12-11 19:07:50 -0600

    Rm outdated todo.

--- src/main/kotlin/org/mozilla/github_bot/ext/String.kt
+++ src/main/kotlin/org/mozilla/github_bot/ext/String.kt
@@ -2,4 +2,4 @@ package org.mozilla.github_bot.ext
 
 import okhttp3.HttpUrl
 
-fun String?.toHttpUrl() = this?.let { HttpUrl.parse(it) } // todo: test.
+fun String?.toHttpUrl() = this?.let { HttpUrl.parse(it) }
