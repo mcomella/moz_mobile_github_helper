1050591d31921bddf0c8075972556704122f4d87
Michael Comella <michael.l.comella@gmail.com>
2017-12-11 19:11:02 -0600

    Commits -> GithubCommits.

--- src/main/kotlin/org/mozilla/github_bot/GithubCommits.kt
+++ src/main/kotlin/org/mozilla/github_bot/GithubCommits.kt
@@ -1,15 +1,14 @@
 package org.mozilla.github_bot
 
-private val issueRegexes = arrayOf( // todo: constant
+private val issueRegexes = arrayOf(
         """^Issue\s*#([0-9]+)""",
         """^Closes\s*#([0-9]+)""",
         """\(#([0-9]+)\)\s*.?$"""
 ).map { it.toRegex(RegexOption.IGNORE_CASE) }
 
-object Commits { // todo: name.
+object GithubCommits {
     fun extractIssueNumFromCommitMsg(commitMsg: String) = issueRegexes.mapNotNull {
         it.find(commitMsg.trim())?.groupValues?.get(1)?.toLong()
     }.firstOrNull()
 }
-
 
--- src/test/kotlin/org/mozilla/github_bot/GithubCommitsTest.kt
+++ src/test/kotlin/org/mozilla/github_bot/GithubCommitsTest.kt
@@ -4,61 +4,61 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-class CommitsTest {
+class GithubCommitsTest {
 
     @Test
     fun `Extracts issue number from "Issue #" prefix`() {
         val expectedIssueNum = 1776L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Issue #$expectedIssueNum: declared independence."))
     }
 
     @Test
     fun `Extracts issue number from "Closes #" prefix`() {
         val expectedIssueNum = 1787L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Closes #$expectedIssueNum: created a Constitution"))
     }
 
     @Test
     fun `Extracts issue number from "(#)" suffix`() {
         val expectedIssueNum = 1777L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Approve articles of confederation (#$expectedIssueNum)"))
     }
 
     @Test
     fun `Extracts issue number from "(#)" suffix with period`() {
         val expectedIssueNum = 1777L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Approve articles of confederation (#$expectedIssueNum)."))
     }
 
     @Test
     fun `Extracts issue number from "(#)" suffix with period and whitespace`() {
         val expectedIssueNum = 1777L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Approve articles of confederation (#$expectedIssueNum)  ."))
     }
 
     @Test
     fun `Extracts issue number while trimming whitespace`() {
         val expectedIssueNum = 1999L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "   Issue #1999: add readme    "))
     }
 
     @Test
     fun `Extracts issue number without whitespace between prefix and number`() {
         val expectedIssueNum = 1999L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Issue#1999: add readme"))
     }
 
     @Test
     fun `Extracts issue number with extra whitespace between prefix and number`() {
         val expectedIssueNum = 1999L
-        assertEquals(expectedIssueNum, Commits.extractIssueNumFromCommitMsg(
+        assertEquals(expectedIssueNum, GithubCommits.extractIssueNumFromCommitMsg(
                 "Issue      #1999: add readme"))
     }
 }
